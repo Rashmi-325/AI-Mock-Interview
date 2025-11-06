4437fbf693006e99d811585c06c62ac4233d58cc
RashmiPriya <145137004+Rashmi-325@users.noreply.github.com>
2025-11-06 09:58:56 +0530

    Delete backend/app directory

--- backend/app/db.py
+++
@@ -1,8 +0,0 @@
-from motor.motor_asyncio import AsyncIOMotorClient
-from .config import settings
-
-client = AsyncIOMotorClient(settings.MONGO_URI)
-db = client.get_default_database()
-users = db["users"]
-sessions = db["sessions"]
-reports = db["reports"]
